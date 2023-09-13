316a22c9f1d7f73d6ab4da11abc99ac0cc2e9ea9
victorbutron <victoralejandrobutron@gmail.com>
2023-09-13 18:52:51 +0200

    my script

---
+++ 00_setup.R
@@ -0,0 +1,4 @@
+# Set-up your script ------------------------------------------------------
+
+# install.packages(c("tidyverse", "gapminder", "pacman")) # uncomment if already installed
+pacman::p_load(tidyverse, gapminder)
